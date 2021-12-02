0278f40e8a6ad12c123cf202aea1ab5b1f9c80e0
Zevgor <hallzacharyj@gmail.com>
2021-12-02 09:19:11 -0500

    Dec 2 Recommit

--- Dec 2/Dec 2 Part 2.py
+++ Dec 2/Dec 2 Part 2.py
@@ -13,9 +13,9 @@ for n in rawlist:
     coord = n.split()
     if coord[0] == 'down':
         aim = aim + int(coord[1])
-    if coord[0] == 'up':
+    elif coord[0] == 'up':
         aim = aim - int(coord[1])        
-    if coord[0] == 'forward':
+    elif coord[0] == 'forward':
         pos[0] = pos[0] + int(coord[1])
         pos[1] = pos[1] + (int(coord[1])*aim)
 
