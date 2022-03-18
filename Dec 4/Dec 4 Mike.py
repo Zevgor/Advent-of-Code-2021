8611b8dcb03d0d1aad4978f4b34da831005409c6
Zevgor <hallzacharyj@gmail.com>
2022-03-17 21:15:37 -0400

    Update March 17 2022

---
+++ Dec 4/Dec 4 Mike.py
@@ -0,0 +1,126 @@
+import sys
+import re
+import pprint
+
+# global variables
+width = 5
+height = 5
+draws = []
+boards = []
+
+# Keep the indexes of all winning boards so we can skip them for efficiency reasons and to also keep track of when we've found the ULTIMATE LOSER
+winningBoards = []
+
+# Open the data file and extract our data
+def getData(runFlag):
+    print('Opening input file...')
+
+    with open('Dec 4\Dec 4 input.txt') as file:
+        lines = file.readlines()
+
+    # The first line is the numbers drawn
+    global draws
+    draws = lines[0].rstrip().split(",")
+
+    # All following lines are the bingo boards
+    board = []
+    row = []
+    rowsConsumed = 0
+    for index, line in enumerate(lines[2:]):
+        if len(line) > 1:
+            # We have a line with real information in it. Scrub out double spaces, split into a list, then append it to the board
+            line = re.sub(' +', ' ', line)
+            row = line.strip().split(" ")
+            board.append(row)
+            rowsConsumed = rowsConsumed + 1
+
+            # Now check to see if we've consumed a full board
+            if rowsConsumed == height:
+                boards.append(board)
+                board = []
+                row = []
+                rowsConsumed = 0
+
+# Check the x and y axis of the board to see if we have a bingo in either direction
+def checkBingo(boardNum, x, y):
+    board = boards[boardNum]
+    # x-axis check
+    hitCount = 0
+    for xIter in range(5):
+        if board[y][xIter] == 'X':
+            hitCount = hitCount + 1
+            if hitCount == 5:
+                #Bingo!  Add the boardNum to the winner's list.  If it's the last board, we'll return True.  Otherwise we'll pretend the game is still going
+                winningBoards.append(boardNum)
+                if len(winningBoards) < len(boards):
+                    return False
+                else:
+                    return True
+        else:
+            # Miss! Reset the hit counter and break the loop
+            hitCount = 0
+            break
+    # y-axis check
+    for yIter in range(5):
+        if board[yIter][x] == 'X':
+            hitCount = hitCount + 1
+            if hitCount == 5:
+                #Bingo!  Add the boardNum to the winner's list.  If it's the last board, we'll return True.  Otherwise we'll pretend the game is still going
+                winningBoards.append(boardNum)
+                if len(winningBoards) < len(boards):
+                    return False
+                else:
+                    return True
+        else:
+            # Miss! There's nothing left to do so return False
+            return False
+
+# Check the board to see if the number drawn gives us a bingo
+def checkBoard(boardNum, draw, board):
+    for y, row in enumerate(board):
+        for x, cell in enumerate(row):
+            if draw == cell:
+                # We have a hit! Mark the board then check for a bingo!
+                boards[boardNum][y][x] = 'X'
+                hasWon = checkBingo(boardNum, x, y)
+                if hasWon:
+                    return "Bingo"
+                
+# Play bingo
+def playBingo(draws, boards):
+    # Iterate through our draws and pass the number to each bingo board
+    for draw in draws:
+        numSkips = 0
+        for boardNum, board in enumerate(boards):
+            # Check to see if the board has already won.  If it has, skip it
+            if boardNum in winningBoards:
+                numSkips = numSkips + 1
+                continue
+            loseFlag = checkBoard(boardNum, draw, board)
+            if loseFlag == "Bingo":
+                return draw, board
+    # We shouldn't be able to reach this line
+    return None, None
+
+# Calculate the final answer
+def calculateFinalAnswer(winningNumber, losingBoard):
+    # First find the sum of all unmarked numbers
+    sum = 0
+    for row in losingBoard:
+        for cell in row:
+            if cell != "X":
+                sum = sum + int(cell)
+
+    # Now multiply the sum with the winning number
+    return sum * int(winningNumber)
+# Are we running against test or input?
+runFlag = "input"
+
+getData(runFlag)
+
+winningNumber, losingBoard = playBingo(draws, boards)
+print("Winning number: " + winningNumber)
+finalAnswer = calculateFinalAnswer(winningNumber, losingBoard)
+print(losingBoard)
+print("Final answer: " + str(finalAnswer))
+print("Done!")
---
+++ Dec 4/Dec 4.py
@@ -0,0 +1,111 @@
+import sys
+
+listfile = 'Dec 4\Dec 4 input.txt'
+
+def remove_items(list,item):
+    res = [i for i in list if i != item]
+    return res
+
+def print_winner(bingocard,drawnum):
+    answer = 0
+    for row in bingocard:
+        print(row[0],row[1],row[2],row[3],row[4])
+        row = remove_items(row,'X')
+        print(row)
+        for num in row:
+            answer = answer + int(num)
+    answer = answer * int(drawnum)
+    print ("answer",answer)
+
+lines = []
+with open(listfile) as f:
+    lines = f.readlines()
+
+# count = 0
+# for line in lines:
+#     count += 1
+#     print(f'line {count}: {line}')
+
+#process 1st line into it's own list
+draworder = lines[0].split(',')
+del lines[0]
+
+#cleanup blank lines
+lines = remove_items(lines,"\n")
+
+cardlist = []
+cardrows = []
+
+cardrowcount = 0
+#load cards
+for line in lines:
+    #remove newline
+    line = line.strip()
+    
+    #add row to rowlist
+    numlist = line.split(' ')
+    numlist = remove_items(numlist,'')
+    cardrows.append(numlist)
+
+    #iterate counter
+    cardrowcount += 1
+
+        #if 5th row (4), reset to 1 for new card
+    if cardrowcount > 4:
+        cardrowcount = 0
+        cardlist.append(cardrows.copy())
+        cardrows.clear()
+
+#at this point we have a 2d array, cardlist, arranges as such:
+
+#[XX XX XX XX XX
+# XX XX XX XX XX
+# XX XX XX XX XX
+# XX XX XX XX XX
+# XX XX XX XX XX] x 100
+
+for idxdn,drawnum in enumerate(draworder):
+    #print('checking',drawnum)
+    for idxbc,bingocard in enumerate(cardlist):
+        #print('is it in here?',bingocard,idxbc)
+        for idxcr,cardrow in enumerate(bingocard):
+            for idxrn,rownum in enumerate(cardrow):
+                #print('row',idxcr,'num',rownum)
+                if rownum == drawnum:
+                    #print('hit!',rownum,'=',drawnum,'on card',idxbc,'row',idxcr,'num',idxrn)
+                    cardlist[idxbc][idxcr][idxrn] = 'X'
+    #check for any completed cards
+    for hitcard in cardlist:
+        #print_card(hitcard)
+        winner = 1
+        for hitrow in hitcard:            
+            #check rows
+            winner = 1
+            for hitnum in hitrow:
+                if hitnum != 'X':
+                    winner = 0
+            if winner == 1:
+                print("HORIZONTAL WINNER!")
+                print_winner(hitcard,drawnum)
+                sys.exit()
+        winner = 1
+        for n in range(0,5):
+            #print (hitcard[0][n],hitcard[1][n],hitcard[2][n],hitcard[3][n],hitcard[4][n],)
+            winner = 1
+            if hitcard[0][n] != 'X':
+                winner = 0
+            if hitcard[1][n] != 'X':
+                winner = 0
+            if hitcard[2][n] != 'X':
+                winner = 0
+            if hitcard[3][n] != 'X':
+                winner = 0
+            if hitcard[4][n] != 'X':
+                winner = 0
+            if winner == 1:
+                break
+        #print (winner,'checking winner')
+        if winner == 1:
+            print("VERTICAL WINNER!")
+            print_winner(hitcard,drawnum)
+            sys.exit()
